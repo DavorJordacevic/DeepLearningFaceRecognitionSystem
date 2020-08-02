10bf53d1c01f4248295196de4207a2236263524a
Davor Jordacevic <davor.jordacevic@fincore.com>
2020-08-02 14:39:35 +0200

    code refactor: mtcnn and haar detector

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+import cv2
+import mtcnn
 import pyfiglet
 import argparse
+import tensorflow as tf
 from utils import dbase
 from utils import config
 from utils.videoProcessor import VideoProcessor
@@ -8,21 +12,36 @@ if __name__ == '__main__':
     ascii_banner = pyfiglet.figlet_format("F R     A P P", font="slant")
     print(ascii_banner)
 
+    #os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
+
+    print("OpenCV version: ", cv2.__version__)
+    print("TensorFlow version: ", tf.__version__, "  GPU avaiable: ", tf.config.list_physical_devices('GPU'))
+
     parser = argparse.ArgumentParser(description='Process some arguments.')
     parser.add_argument('--cdp', type=str, help='the path to config file')
+    parser.add_argument('--video_source', type=str, help='video source')
 
     args = parser.parse_args()
     config_path = args.cdp
+    video_source = args.video_source if args.video_source!="0" else 0
 
     cfg = config.readConfig(config_path)
     db_conn, db = dbase.dbConnect(cfg["host"], cfg["port"], cfg["name"], cfg["user"], cfg["password"])
 
     dbase.readDescriptors(db)
 
-    video = VideoProcessor(0, cfg["face_detector"], cfg["face_det_model_path"],
-                           cfg["face_recognition"], cfg["face_reco_model_path"], cfg["threshold"],
-                           cfg["tracker"], cfg["tracker_model_path"],
-                           cfg["face_antispoofing"], cfg["face_antispoofing_model_path"],
-                           )
+    video = VideoProcessor(video_source, cfg)
 
-    video.capture()
+    if cfg["write_to_file"] == "false":
+        video.capture()
+    else:
+        video.capture_and_write()
--- utils/videoProcessor.py
+++ utils/videoProcessor.py
@@ -1,48 +1,170 @@
 import cv2
-import matplotlib.pyplot as plt
+import time
+import mtcnn
+import numpy as np
+from collections import defaultdict
 
 class VideoProcessor:
-  def __init__(self, path, detector, detector_path,
-               recognizer, recognizer_path, threshold,
-               tracker, tracker_path,
-               antispoofing, antispoofing_path):
+    def __init__(self, video_source, cfg):
 
-    self.path = path
-    self.detector = detector
-    self.detector_path = detector_path
-    self.recognizer = recognizer
-    self.recognizer_path = recognizer_path
-    self.threshold = threshold
-    self.tracker = tracker
-    self.tracker_path = tracker_path
-    self.antispoofing = antispoofing
-    self.antispoofing_path = antispoofing_path
+        self.video_source = video_source
+        self.detector_type = cfg["face_detector"]
+        self.dnn_detector_path = cfg["face_det_model_path"]
+        self.haar_model_file = cfg["opencv_face_detector_uint8"]
+        self.haar_config_file = cfg["opencv_face_detector_pbtxt"]
+        self.recognizer = cfg["face_recognition"]
+        self.recognizer_path = cfg["face_reco_model_path"]
+        self.threshold = cfg["threshold"]
+        self.tracker = cfg["tracker"]
+        self.tracker_path = cfg["tracker_model_path"]
+        self.antispoofing = cfg["face_antispoofing"]
+        self.antispoofing_path = cfg["face_antispoofing_model_path"]
 
-  def capture(self):
-    # Create a VideoCapture object and read from input file
-    # If the input is taken from the camera, pass 0 instead of the video file name.
-    self.cap = cv2.VideoCapture(self.path)
+        if self.detector_type == "MTCNN":
+            if self.dnn_detector_path == "":
+                print("[INFO] Loading MTCNN detection model...")
+                self.detector = mtcnn.MTCNN()
+                print("[INFO] MTCNN detection model loaded.")
+                print("MTCNN version: ", mtcnn.__version__)
 
-    # Check if video capture object is openanned successfully
-    if (self.cap.isOpened() == False):
-        print("Error opening video stream or file")
+        if self.detector_type == "HAAR":
+            # load our serialized model from disk
+            print("[INFO] Loading Haar detection model...")
+            # Here we need to read our pre-trained neural net created using Tensorflow
+            self.detector = cv2.dnn.readNetFromTensorflow(self.haar_model_file, self.haar_config_file)
+            self.min_confidence = 0.5  # minimum probability to filter weak detections
+            print("[INFO] Haar detection model loaded.")
 
-    # Read until video is completed
-    while (self.cap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = self.cap.read()
-        if ret == True:
-            # Display the resulting frame
-            cv2.imshow('Video stream', frame)
+    def rescale_frame(self, frame, percent=75):
+        width = int(frame.shape[1] * percent / 100)
+        height = int(frame.shape[0] * percent / 100)
+        dim = (width, height)
+        return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
-        # Press Q on keyboard to  exit
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
+    def capture(self):
+        # Create a VideoCapture object and read from input file
+        # If the input is taken from the camera, pass 0 instead of the video file name.
+        self.cap = cv2.VideoCapture(self.video_source)
 
-        #fps = self.cap.get(cv2.CAP_PROP_FPS)
-        #print(fps)
+        # Check if video capture object is openanned successfully
+        if (self.cap.isOpened() == False):
+            print("Error opening video stream or file")
 
-    # When everything done, release the video capture object
-    self.cap.release()
-    # Closes all the frames
-    cv2.destroyAllWindows()
+        tracker = cv2.TrackerGOTURN_create()
+
+        # Read until video is completed
+        while (self.cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            #frame = self.rescale_frame(frame, 25)
+
+            if self.detector_type == "HAAR":
+                (h, w) = frame.shape[:2]
+                blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+                self.detector.setInput(blob)
+                # Runs forward pass to compute outputs of layers listed in outBlobNames.
+                detections = self.detector.forward()
+
+                for i in range(0, detections.shape[2]):
+                    # extract the confidence (probability) associated with the prediction
+                    confidence = detections[0, 0, i, 2]
+
+                    # filter out weak detections by ensuring the `confidence` is
+                    # greater than the minimum confidence
+                    if confidence > self.min_confidence:
+                        # compute the (x, y)-coordinates of the bounding box for the object
+                        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                        (startX, startY, endX, endY) = box.astype("int")
+
+                        # draw the bounding box of the face along with the associated
+                        # probability
+                        text = "{:.2f}%".format(confidence * 100)
+                        y = startY - 10 if startY - 10 > 10 else startY + 10
+                        cv2.rectangle(frame, (startX, startY), (endX, endY),
+                                      (0, 0, 255), 2)
+                        cv2.putText(frame, text, (startX, y),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+            if self.detector_type == "MTCNN":
+                if self.dnn_detector_path == "":
+                    faces = self.detector.detect_faces(frame)
+                    for face in faces:
+                        # get coordinates
+                        x, y, width, height = face['box']
+
+                        cv2.rectangle(frame, (x, y), (x + width, y + height), color=(0, 0, 255), thickness=2)
+
+                        for key, value in face['keypoints'].items():
+                            # create and draw dots
+                            cv2.circle(frame, value, radius=2, color=(0, 0, 255), thickness=1)
+
+                        text = "{:.2f}%".format(face['confidence'] * 100)
+                        cv2.putText(frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+            # Initialize tracker with first frame and bounding box
+            ok = tracker.init(frame, tuple(box))
+
+            if ret == True:
+                # Display the resulting frame
+                cv2.imshow('Video stream', frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+            #fps = self.cap.get(cv2.CAP_PROP_FPS)
+            #print(fps)
+
+        # When everything done, release the video capture object
+        self.cap.release()
+        # Closes all the frames
+        cv2.destroyAllWindows()
+
+    def capture_and_write(self):
+        # Create a VideoCapture object and read from input file
+        # If the input is taken from the camera, pass 0 instead of the video file name.
+        self.cap = cv2.VideoCapture(self.video_source)
+
+        # Check if video capture object is openanned successfully
+        if (self.cap.isOpened() == False):
+            print("Error opening video stream or file")
+
+        # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+        # We convert the resolutions from float to integer.
+        frame_width = int(self.cap.get(3))
+        frame_height = int(self.cap.get(4))
+
+        # Define the codec and create VideoWriter object.The output is stored in 'output_py.avi' file.
+        out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
+
+        # Read until video is completed
+        while (self.cap.isOpened()):
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            if ret == True:
+                # Display the resulting frame
+                cv2.imshow('Video stream', frame)
+                # Write the frame into the file 'output.avi'
+                out.write(frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+            #fps = self.cap.get(cv2.CAP_PROP_FPS)
+            #print(fps)
+
+        # When everything done, release the video capture object
+        self.cap.release()
+        out.release()
+        # Closes all the frames
+        cv2.destroyAllWindows()
+
+    def dnn_detect(self):
+        pass
+
+    def mtcnn_detect(self):
+        pass
+
+    def haar_detect(self):
+        pass
